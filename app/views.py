3bee2f36efa0e5ded2b077d641432bd992d620d3
sanctusgee <godwin.dsg@gmail.com>
2015-02-17 11:29:50 -0700

    minor changes

--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
-from flask.ext.login import login_user, logout_user, current_user, \
-    login_required
+from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
 from .forms import LoginForm
 from .models import User
@@ -23,12 +22,12 @@ def index():
     user = g.user
     posts = [
         {
-            'author': {'nickname': 'John'},
-            'body': 'Beautiful day in Portland!'
+            'author': {'nickname': 'Jose'},
+            'body': 'Traveling and rock-climbing!'
         },
         {
             'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'body': 'Swimming, traveling and reading!'
         }
     ]
     return render_template('index.html',
