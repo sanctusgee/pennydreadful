9fd15f323af14db6de5fe41098d61f66ed3ed3d3
sanctusgee <godwin.dsg@gmail.com>
2015-02-16 20:57:52 -0700

    user logins added

--- app/views.py
+++ app/views.py
@@ -1,12 +1,18 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
 
+@app.before_request
+def before_request():
+    g.user = current_user
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user = {'nickname': 'Miguel'}
+    user = ['James', 'Miguel']
     posts = [
         {
             'author': {'nickname': 'John'},
@@ -14,7 +20,7 @@ def index():
         },
         {
             'author': {'nickname': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
+            'body': 'House of Cards is an awesome show!'
         }
     ]
     return render_template('index.html',
@@ -34,3 +40,30 @@ def login():
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email == "":
+        flash('Invalid login. Please try again.')
+        return redirect(url_for('login'))
+    user = User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname = resp.nickname
+        if nickname is None or nickname == "":
+            nickname = resp.email.split('@')[0]
+        user = User(nickname=nickname, email=resp.email)
+        db.session.add(user)
+        db.session.commit()
+    remember_me = False
+    if 'remember_me' in session:
+        remember_me = session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return redirect(request.args.get('next') or url_for('index'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
