6230f3533a3c520c5a25ea34b95b6b853b0dff98
sanctusgee <godwin.dsg@gmail.com>
2015-02-17 18:04:36 -0700

    added stylesheets

--- app/views.py
+++ app/views.py
@@ -1,8 +1,9 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from .forms import LoginForm
+from .forms import LoginForm, EditForm
 from .models import User
+from datetime import datetime
 
 
 @lm.user_loader
@@ -13,6 +14,10 @@ def load_user(id):
 @app.before_request
 def before_request():
     g.user = current_user
+    if g.user.is_authenticated():
+        g.user.last_seen = datetime.utcnow()
+        db.session.add(g.user)
+        db.session.commit()
 
 
 @app.route('/')
@@ -91,3 +96,21 @@ def after_login(resp):
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+from forms import LoginForm, EditForm
+
+@app.route('/edit', methods=['GET', 'POST'])
+@login_required
+def edit():
+    form = EditForm()
+    if form.validate_on_submit():
+        g.user.nickname = form.nickname.data
+        g.user.about_me = form.about_me.data
+        db.session.add(g.user)
+        db.session.commit()
+        flash('Your changes have been saved.')
+        return redirect(url_for('edit'))
+    else:
+        form.nickname.data = g.user.nickname
+        form.about_me.data = g.user.about_me
+    return render_template('edit.html', form=form)
